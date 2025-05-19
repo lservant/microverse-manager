b14a9345b862fc19480ea541add6c5c002d1cd98
Louis Servant <louservant@gmail.com>
2025-05-18 20:01:04 -0500

    genericised

--- bottle/bottle_cell.gd
+++ bottle/bottle_cell.gd
@@ -18,52 +18,54 @@ func move_water() -> void:
     return
   var water_dropped = false
   if not is_bottom():
-    water_dropped = drop_water()
-  if water.amount <= 12 or water_dropped:
+    water_dropped = drop_resource(ResourcePool.ResourceType.WATER)
+  if water.amount < 3 or water_dropped:
     return
-  spill_water()
+  spill_resource(ResourcePool.ResourceType.WATER)
 
-const MAX_WATER: int = 100
-func spill_water() -> void:
-  var water = resources.get_resource(ResourcePool.ResourceType.WATER)
+const RESOURCE_LIMIT: int = 100
+func spill_resource(resource_type: ResourcePool.ResourceType) -> void:
+  var rsrc = resources.get_resource(resource_type)
 
   if neighbors.left != null and neighbors.right != null:
-    var left_water = neighbors.left.resources.get_resource(ResourcePool.ResourceType.WATER)
-    var right_water = neighbors.right.resources.get_resource(ResourcePool.ResourceType.WATER)
-    var total: int = left_water.amount + right_water.amount + water.amount
+    var left_rsrc = neighbors.left.resources.get_resource(resource_type)
+    var right_rsrc = neighbors.right.resources.get_resource(resource_type)
+    var total: int = left_rsrc.amount + right_rsrc.amount + rsrc.amount
     var avg: int = total / 3
-    if left_water.amount < MAX_WATER and left_water.amount < avg:
-      var amount_to_move = avg - left_water.amount
-      if amount_to_move <= water.amount:
-        move_resource(ResourcePool.ResourceType.WATER, amount_to_move, neighbors.left)
-    if right_water.amount < MAX_WATER and right_water.amount < avg:
-      var amount_to_move = avg - right_water.amount
-      if amount_to_move <= water.amount:
-        move_resource(ResourcePool.ResourceType.WATER, amount_to_move, neighbors.right)
-  elif neighbors.left != null:
-    var left_water = neighbors.left.resources.get_resource(ResourcePool.ResourceType.WATER)
-    if left_water.amount < MAX_WATER && left_water.amount < water.amount:
-      var total: int = left_water.amount + water.amount
-      var amount_to_move = total / 2 - left_water.amount
-      if amount_to_move <= water.amount:
-        move_resource(ResourcePool.ResourceType.WATER, amount_to_move, neighbors.left)
-  elif neighbors.right != null:
-    var right_water = neighbors.right.resources.get_resource(ResourcePool.ResourceType.WATER)
-    if right_water.amount < MAX_WATER && right_water.amount < water.amount:
-      var total: int = right_water.amount + water.amount
-      var amount_to_move = total / 2 - right_water.amount
-      if amount_to_move <= water.amount:
-        move_resource(ResourcePool.ResourceType.WATER, amount_to_move, neighbors.right)
+    print("Left: %s, Right: %s, Avg: %s" % [left_rsrc.amount, right_rsrc.amount, avg])
+    if left_rsrc.amount < RESOURCE_LIMIT and left_rsrc.amount < avg:
+      var amount_to_move = avg - left_rsrc.amount
+      if amount_to_move <= rsrc.amount:
+        move_resource(resource_type, amount_to_move, neighbors.left)
+    if right_rsrc.amount < RESOURCE_LIMIT and right_rsrc.amount < avg:
+      var amount_to_move = avg - right_rsrc.amount
+      if amount_to_move <= rsrc.amount:
+        move_resource(resource_type, amount_to_move, neighbors.right)
 
-func drop_water()-> bool:
-  var water = resources.get_resource(ResourcePool.ResourceType.WATER)
-  var bottom_water = neighbors.bottom.resources.get_resource(ResourcePool.ResourceType.WATER)
-  if bottom_water.amount >= MAX_WATER:
+  elif neighbors.left != null:
+    var left_rsrc = neighbors.left.resources.get_resource(resource_type)
+    spill_one_side(left_rsrc, rsrc)
+
+  elif neighbors.right != null:
+    var right_rsrc = neighbors.right.resources.get_resource(resource_type)
+    spill_one_side(right_rsrc, rsrc)
+
+func spill_one_side(side_rsrc: ResourceInfo, rsrc: ResourceInfo):
+  if side_rsrc.amount < RESOURCE_LIMIT && side_rsrc.amount < rsrc.amount:
+    var total: int = side_rsrc.amount + rsrc.amount
+    var amount_to_move = total / 2 - side_rsrc.amount
+    if amount_to_move <= rsrc.amount:
+      move_resource(rsrc.resource_type, amount_to_move, neighbors.right)
+
+func drop_resource(resource_type: ResourcePool.ResourceType) -> bool:
+  var rsrc = resources.get_resource(resource_type)
+  var bottom_rsrc = neighbors.bottom.resources.get_resource(resource_type)
+  if bottom_rsrc.amount >= RESOURCE_LIMIT:
     return false
-  var amount_to_move = MAX_WATER - bottom_water.amount
-  if amount_to_move > water.amount:
-    amount_to_move = water.amount
-  move_resource(ResourcePool.ResourceType.WATER, amount_to_move, neighbors.bottom)
+  var amount_to_move = RESOURCE_LIMIT - bottom_rsrc.amount
+  if amount_to_move > rsrc.amount:
+    amount_to_move = rsrc.amount
+  move_resource(resource_type, amount_to_move, neighbors.bottom)
   return true
 
 func update_resources() -> void:
